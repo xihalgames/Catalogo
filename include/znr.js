c86b89473f9f7bd05554b021bdc2aad975a6b887
mxgamma <mxgamma.main@gmail.com>
2020-10-11 23:20:15 -0500

    Update znr.js

--- include/znr.js
+++ include/znr.js
@@ -2,14 +2,15 @@ var diceSet = new Array()
 diceSet[0] =  new Array({
     serie: "D",
     modelo: "01",
+    stock: "10",
     nombre: "Escalofrio1",
-    desc: "temporal",
-    stock: "10"
+    desc: "temporal"
+
 })
 diceSet[1] =  new Array({
     serie: "D",
     modelo: "02",
+    stock: "10",
     nombre: "Escalofrio2",
-    desc: "temporal",
-    stock: "10"
+    desc: "temporal"
 })
