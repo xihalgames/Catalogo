be66a4882213810bbac779f8ba774a4a17aa318f
mxgamma <mxgamma.main@gmail.com>
2020-10-11 23:20:27 -0500

    Update diceStock.js

--- include/diceStock.js
+++ include/diceStock.js
@@ -2,12 +2,15 @@ var diceSet = new Array()
 diceSet[0] =  new Array({
     serie: "D",
     modelo: "01",
+    stock: "10",
     nombre: "Escalofrio1",
     desc: "temporal"
+
 })
 diceSet[1] =  new Array({
     serie: "D",
     modelo: "02",
+    stock: "10",
     nombre: "Escalofrio2",
     desc: "temporal"
 })
